7d74872c0a96e618ecf290babb3754425df54966
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-17 23:28:17 -0600

    Working without viewModel

---
+++ SearchbarDemo/SearchbarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchbarDemoApp.swift
+//  SearchbarDemo
+//
+//  Created by Marco Alonso Rodriguez on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SearchbarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
